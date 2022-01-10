226e10d1f504e2601ce5bb9397a2a729c2af74b6
Daniel <sonusbeat@gmail.com>
2022-01-10 01:12:45 -0600

    Lazy Load

--- src/01-lazy-load/Pages/LazyPage2.tsx
+++ src/01-lazy-load/Pages/LazyPage2.tsx
@@ -2,6 +2,11 @@ export const LazyPage2 = () => {
   return (
     <div>
       <h1 className="display-1 cyan">Lazy Page 2</h1>
+      <div className="alert alert-info text-center fw-bolder">
+        This is loaded by Lazy Load
+      </div>
     </div>
   );
 };
+
+export default LazyPage2;
