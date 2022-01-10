781ff59c5e84d7ce736d093f45b390e116480203
Daniel <sonusbeat@gmail.com>
2022-01-09 22:41:32 -0600

    App Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
+import Navigation from './Routes/Navigation';
+
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="text-center cyan display-1">
-        React App Advanced
-      </h1>
-      <hr />
-    </div>
+    <>
+      <Navigation />
+    </>
   );
 };
 
